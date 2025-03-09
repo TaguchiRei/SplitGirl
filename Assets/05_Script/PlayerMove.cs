21f09b46a668c7dbe719a9ba3757c0fd5930cd73
TaguchiRei <ren.len2426@gmail.com>
2025-03-10 01:53:07 +0900

    これから仮想パッドを作成開始

--- Assets/05_Script/PlayerMove.cs
+++ Assets/05_Script/PlayerMove.cs
@@ -1,19 +1,24 @@
-using System;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed;
+    private static readonly int Move = Animator.StringToHash("Move");
+    private static readonly int Run = Animator.StringToHash("Run");
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int OnGround = Animator.StringToHash("OnGround");
+    [SerializeField] float _walkSpeed;
+    [SerializeField] float _runSpeed;
     [SerializeField] float _jumpForce;
     
+    [SerializeField] Animator _animator;
     [SerializeField] Rigidbody _rigidBody;
 
     private bool _onGround;
 
     private void Start()
     {
+        _onGround = true;
 #if UNITY_EDITOR
         Cursor.lockState = CursorLockMode.Locked;
 #endif
@@ -26,6 +31,7 @@ public class PlayerMove : MonoBehaviour
         //unityEditor上ではキーボード操作できるようにする
         if (Input.GetKeyDown(KeyCode.Space) && _onGround)
         {
+            _animator.SetTrigger(Jump);
             //ジャンプの処理
             _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             _onGround = false;
@@ -34,7 +40,31 @@ public class PlayerMove : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y + Input.GetAxisRaw("Mouse X"), 0);
         //移動する
         Vector3 move = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized);
-        _rigidBody.linearVelocity = new Vector3(move.x * _moveSpeed, _rigidBody.linearVelocity.y, move.z * _moveSpeed);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            move *= _runSpeed;
+            _animator.SetBool(Run, true);
+        }
+        else
+        {
+            move *= _walkSpeed;
+            _animator.SetBool(Run, false);
+        }
+        
+        if (move.magnitude != 0)
+        {
+            _animator.SetBool(Move, true);
+        }
+        else
+        {
+            _animator.SetBool(Move, false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _animator.SetTrigger(Jump);
+        }
+        _rigidBody.linearVelocity = new Vector3(move.x * _walkSpeed, _rigidBody.linearVelocity.y, move.z * _walkSpeed);
 #endif
     }
 
@@ -43,6 +73,7 @@ public class PlayerMove : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             _onGround = true;
+            _animator.SetTrigger(OnGround);
         }
     }
 }
