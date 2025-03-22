f32be6f9116eb6e3400f4d6047889d2c74a9c62c
TaguchiRei <ren.len2426@gmail.com>
2025-03-22 21:56:15 +0900

    [Add]仮実装。あとで作り直す。

--- Assets/05_Script/GameObj/FootSwitch.cs
+++ Assets/05_Script/GameObj/FootSwitch.cs
@@ -1,9 +1,26 @@
+using System;
 using UnityEngine;
 
 public class FootSwitch : InteractObjectBase
 {
     public override void Interact()
     {
-        throw new System.NotImplementedException();
+        transform.position = new Vector3(transform.position.x, transform.position.y-0.3f, transform.position.z);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("MainPlayer")|| other.gameObject.CompareTag("SubPlayer"))
+        {
+            Interact();
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("MainPlayer")|| other.gameObject.CompareTag("SubPlayer"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y+0.3f, transform.position.z);
+        }
     }
 }
