fc9499c4a081beff8e415d3ff59b86a0f78a8c2d
TaguchiRei <ren.len2426@gmail.com>
2025-03-30 00:10:28 +0900

    [Add]画面内に映っているかを調べるメソッドを作成

--- Assets/05_Script/GameObj/InGameManager.cs
+++ Assets/05_Script/GameObj/InGameManager.cs
@@ -2,13 +2,14 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Cysharp.Threading.Tasks;
-using UnityEngine.Serialization;
 
 public class InGameManager : MonoBehaviour
 {
     [SerializeField] private string _subScene;
     [SerializeField] private GameObject _Screen;
     [SerializeField] private PlayerAnimationManager _playerAnimationManager;
+
+    [SerializeField] private float _inScreenStandards;
     
     public Vector2 DirectionVector; //現在の角度ベクトル
     
@@ -17,6 +18,9 @@ public class InGameManager : MonoBehaviour
     public Action LoadedStart;
     
     public bool LoadedFlag;
+    
+    private Camera _mainCamera;
+    private Camera _subCamera;
     
 
     private void Start()
@@ -39,6 +43,18 @@ public class InGameManager : MonoBehaviour
         await SceneManager.LoadSceneAsync(_subScene, LoadSceneMode.Additive).ToUniTask();
         _playerAnimationManager._mainPlayerAnimator = GameObject.FindGameObjectWithTag("MainPlayer").GetComponent<Animator>();
         _playerAnimationManager._subPlayerAnimator = GameObject.FindGameObjectWithTag("SubPlayer").GetComponent<Animator>();
+        var cameras = FindObjectsByType<Camera>(FindObjectsSortMode.None);
+        foreach (var camera in cameras)
+        {
+            if (camera.CompareTag("MainCamera"))
+            {
+                _mainCamera = camera;
+            }
+            else if (camera.CompareTag("SubCamera"))
+            {
+                _subCamera = camera;
+            }
+        }
         LoadedStart?.Invoke();
         LoadedFlag = true;
     }
@@ -48,9 +64,22 @@ public class InGameManager : MonoBehaviour
         cameraMode = mode;
     }
 
-    public void CheckInScreen(bool main, Vector3 worldPos)
+    /// <summary>
+    /// 画面内に写っているかを調べるメソッド。ある場合はtrue、無ければfalseを返す
+    /// </summary>
+    /// <param name="main"></param>
+    /// <param name="worldPos"></param>
+    /// <returns></returns>
+    public bool CheckInScreen(bool main, Vector3 worldPos)
     {
-        
+        Vector3 screenPos = main ? _mainCamera.WorldToScreenPoint(worldPos) : _subCamera.WorldToScreenPoint(worldPos);
+        float crossProduct = DirectionVector.x * screenPos.y - DirectionVector.y * screenPos.x;
+        if (main)
+        {
+            return crossProduct > _inScreenStandards;
+        }
+
+        return crossProduct < _inScreenStandards;
     }
     
     public enum CameraMode
