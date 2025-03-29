2f3c550920fe16f2a0ec2f34376253c417449e1b
TaguchiRei <ren.len2426@gmail.com>
2025-03-29 23:43:53 +0900

    [Update]ふむスイッチを本格作成開始

--- Assets/05_Script/GameObj/FootSwitch.cs
+++ Assets/05_Script/GameObj/FootSwitch.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 
 public class FootSwitch : InteractObjectBase
 {
+    private Vector3 _startPos;
+    void Start()
+    {
+        _startPos = transform.position;
+    }
     public override void Interact()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y-0.3f, transform.position.z);
+        transform.position = new Vector3(_startPos.x, _startPos.y - 0.3f, _startPos.z);
         interactObject.Interact();
     }
 
     private void CancelInteract()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y+0.3f, transform.position.z);
+        transform.position = _startPos;
         interactObject.Cancel();
     }
 
@@ -20,6 +25,21 @@ public class FootSwitch : InteractObjectBase
         if (other.gameObject.CompareTag("MainPlayer")|| other.gameObject.CompareTag("SubPlayer"))
         {
             Interact();
+        }
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("MainPlayer"))
+        {
+            foreach (var obj in other.contacts)
+            {
+                
+            }
+        }
+        else if (other.gameObject.CompareTag("SubPlayer"))
+        {
+            
         }
     }
 
